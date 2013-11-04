68486f765a67acd1b36727714e2ed40351897446
dbgoodman <dbg@mit.edu>
2013-11-04 16:34:47 -0500

    Removed Receq.

--- receq/snpeff.sh
+++
@@ -1,6 +0,0 @@
-java -Xmx2G -jar /opt/snpEff/snpEff.jar eff \
-    -i vcf -o vcf \
-    -s ${path}.eff.html \
-    -ud 50 \
-    -no-downstream \
-    ecoli <(perl -pe "s/Chromosome/U00096/g" $path) \
